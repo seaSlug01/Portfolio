bfb2dab8a3c060981ed69a38527c885bbda8b34e
unknown <deekwstaz@gmail.com>
2024-01-21 17:50:45 +0200

    New git?

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+// add the beginning of your app entry
+import 'vite/modulepreload-polyfill'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import App from './App'
+import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop />
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+)
